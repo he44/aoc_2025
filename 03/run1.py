818da8a06af6f39528825a898bb31fe8e6862d71
he44 <johnwatson221bbaker@outlook.com>
2025-12-03 01:18:43 -0800

    Day 3

---
+++ 03/run1.py
@@ -0,0 +1,36 @@
+import sys
+from typing import Optional
+
+
+def main():
+    assert len(sys.argv) == 2, f"Unable to parse args {sys.argv}"
+    input_file = sys.argv[1]
+    contents = open(input_file).readlines()
+    ans = 0
+    for content in contents:
+        line = content.strip()
+        n = len(line)
+        # ml[i] is the largest value in line[:i]
+        # ml[0] is -inf, ml[n] is the max in all
+        # mr[i] is the largest value in line[i:]
+        # mr[0] is the max in all, mr[n] is inf
+        max_on_left = [float("-inf") for _ in range(n  + 1)]
+        max_on_right = [float("-inf") for _ in range(n  + 1)]
+        for i in range(1, n + 1):
+            max_on_left[i] = max(max_on_left[i - 1], int(line[i - 1]))
+        for i in range(n - 1, -1, -1):
+            max_on_right[i] = max(max_on_right[i + 1], int(line[i]))
+        # construct the largest number by picking a i with max on left and max on right
+        max_joltage = 0
+        for i in range(n):
+            max_joltage = max(max_joltage, 10 * max_on_left[i] + max_on_right[i])
+        # print(line)
+        # print(max_on_left)
+        # print(max_on_right)
+        # print(max_joltage)
+        # print("-----")
+        ans += max_joltage
+    print(ans)
+
+if __name__ == "__main__":
+    main()
---
+++ 03/run2.py
@@ -0,0 +1,48 @@
+import sys
+from typing import Optional
+from functools import cache
+
+
+
+
+def main():
+    assert len(sys.argv) == 2, f"Unable to parse args {sys.argv}"
+    input_file = sys.argv[1]
+    contents = open(input_file).readlines()
+    ans = 0
+    for content in contents:
+        line = content.strip()
+        n = len(line)
+        k = 12
+        # dp[i][j] denote the maximum number we can generate
+        # with strings in line[i:] with j digits. So the final answer will be dp[0][12]
+        # Unbounded DP
+        @cache
+        def _dp(start_index: int, length: int, line: str):
+            # No enough characters to generate a string of `length`
+            if start_index + length > n:
+                return float("-inf")
+            # No more characters to pick from.
+            if start_index >= n or length < 0:
+                return float("-inf")
+            # Can only pick one character.
+            if start_index == (n - 1) and length == 1:
+                return int(line[start_index])
+            if length == 0:
+                return 0
+            # We can generate the number based on sub-cases.
+            # Let's pick the max number among line[start_index : i],
+            # and then pick the rest of the numbers in line[i:]
+            max_num_so_far = int(line[start_index])
+            max_joltage = float("-inf")
+            for i in range(start_index + 1, n):
+                max_joltage = max(max_num_so_far * (10 ** (length - 1))
+                    + _dp(i, length - 1, line), max_joltage)
+                max_num_so_far = max(int(line[i]), max_num_so_far)
+            return max_joltage
+        max_j = _dp(0, k, line)
+        ans += max_j
+    print(ans)
+
+if __name__ == "__main__":
+    main()
