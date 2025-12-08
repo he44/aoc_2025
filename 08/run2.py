b6d41688d9a4b48abb1084d1ae97bfe98974728b
he44 <johnwatson221bbaker@outlook.com>
2025-12-08 01:40:51 -0800

    Day 8

---
+++ 08/run2.py
@@ -0,0 +1,95 @@
+import heapq
+import math
+import sys
+from typing import List, Optional
+from dataclasses import dataclass
+
+@dataclass
+class JunctionBox:
+    x: int
+    y: int
+    z: int
+    # Row nubmer in the input file
+    id: int
+    # Group lead id. The id of the first point in this group
+    group_lead_id: int
+    # Child ids. Will be empty unless this is a group lead.
+    child_ids: Optional[List[int]]
+
+
+def compute_distance(p1: JunctionBox, p2: JunctionBox):
+    return math.sqrt(
+        (p1.x - p2.x) ** 2 + 
+        (p1.y - p2.y) ** 2 + 
+        (p1.z - p2.z) ** 2
+    )
+
+
+def main():
+    assert len(sys.argv) == 2, f"Unable to parse args {sys.argv}"
+    input_file = sys.argv[1]
+    contents = open(input_file).readlines()
+    points = []
+    i = 0
+    for content in contents:
+        x, y, z = content.strip().split(",")
+        points.append(JunctionBox(int(x), int(y), int(z), i, i, [i]))
+        i += 1
+
+    num_points = len(points)
+
+    # Let's compute the distance, and put it in  a priority queue.
+    sorted_pairs_by_dist = []
+    heapq.heapify(sorted_pairs_by_dist)
+
+    for start in range(num_points):
+        for end in range(start + 1, num_points):
+            dist = compute_distance(points[start], points[end])
+            # Python heapq is min-heap. We need max-heap behavior here.
+            heapq.heappush(sorted_pairs_by_dist, (dist, start, end))
+
+    count = 0
+    while sorted_pairs_by_dist:
+        dist, start_id, end_id = heapq.heappop(sorted_pairs_by_dist)
+        # print(f"Trying to connect {points[start_id]} and {points[end_id]} with distance {dist}.")
+        start_circuit_lead = points[start_id].group_lead_id
+        end_circut_lead = points[end_id].group_lead_id
+        if start_circuit_lead == end_circut_lead:
+            # Already connected, nothing to do.
+            continue
+
+        start_box = points[start_circuit_lead]
+        end_box = points[end_circut_lead]
+        start_circuit_size = len(start_box.child_ids)
+        end_circut_size = len(end_box.child_ids)
+
+        def _merge(bigger_id: int, smaller_id: int):
+            bigger_box = points[bigger_id]
+            smaller_box = points[smaller_id]
+            # Write group lead
+            smaller_box.group_lead_id = bigger_box.id
+            bigger_box.child_ids = bigger_box.child_ids + smaller_box.child_ids
+            # I forgot to update the `group_lead_id` in `child_ids`... Wasn't seeing
+            # issue with the example text case.
+            # In the end I asked AI to spot this issue for me.
+            for child_id in smaller_box.child_ids:
+                points[child_id].group_lead_id = bigger_id
+            smaller_box.child_ids = []
+
+        if start_circuit_size > end_circut_size:
+            _merge(start_circuit_lead, end_circut_lead)
+        else:
+            _merge(end_circut_lead, start_circuit_lead)
+
+        # print(f"Connected {points[start_circuit_lead]} and {points[end_circut_lead]} with distance {dist}.")
+        if len(start_box.child_ids) == len(points) or len(end_box.child_ids) == len(points):
+            print(f"Done connecting {len(points)} points!")
+            print(f"Last two groups connected: {start_box}, {end_box}")
+            print(f"Last two junction points: {points[start_id]}, {points[end_id]}")
+            print(f"{points[start_id].x * points[end_id].x}")
+            break
+
+
+if __name__ == "__main__":
+    main()
+
